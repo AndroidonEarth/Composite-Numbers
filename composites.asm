64863079baf91465c277165aba75c8783854f8a9
Andrew Swaim <AndroidonEarth@users.noreply.github.com>
2018-07-05 16:01:22 -0700

    Update composites.asm

--- composites.asm
+++ composites.asm
@@ -4,7 +4,7 @@ TITLE Composite Numbers     (composites.asm)
 ; Date: 2/18/2018
 ; Description: A program to calculate composite numbers. First, the user is instructed to enter 
 ;	the number of composites to be displayed, and is prompted to enter an integer in the 
-;	range [1 .. 400]. The user enters a number, n, and the program verifies that 0 < n < 401. 
+;	range [1 .. 400]. The user enters a number 'n', and the program verifies that 0 < n < 401. 
 ;	If n is out of range, the user is re-prompted until s/he enters a value in the specified range. 
 ;	The program then calculates and displays all of the composite numbers up to and including the
 ;	n-th composite. The results are displayed 10 composites per line with at least 3 spaces 
